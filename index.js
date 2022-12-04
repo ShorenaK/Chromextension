13612359b658148ea522ef3d0dda6dd1c2b9248d
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-04 18:03:31 -0500

    renderleads function

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
+let myLeads = []
 const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
-let listItems = ''
 inputBtn.addEventListener('click', function(){
     // push th value from the inputEl into the myleads array
-    // inputEl.value += myLead
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ''
+    renderLeads()
 })
 // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 
-for(let i= 0; i < myLeads.length; i++){
-    //   ulEl.textContent += myLeads[i] + ' '
-    // let li = document.createElement('li')
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
 
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
+function renderLeads(){
+    let listItems = ''
+    for(let i= 0; i < myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }   
 ulEl.innerHTML = listItems
+}
