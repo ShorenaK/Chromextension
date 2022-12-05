836035861850858ac9882aa2314e6e2607448f0b
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-05 09:31:48 -0500

    cleaned files ready to deploy

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', function(){
-    // push th value from the inputEl into the myleads array
+
     myLeads.push(inputEl.value)
     inputEl.value = ''
     renderLeads()
 })
-// ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-
 
 function renderLeads(){
     let listItems = ''
     for(let i= 0; i < myLeads.length; i++){
-        listItems += "<li> <a target='_blank' href='" + myLeads[i] + "'> " + myLeads[i] + "</a> </li>"
+        listItems += 
+        `<li> 
+            <a target='_blank' 
+            href= ${myLeads[i]}> 
+            ${myLeads[i]}
+             </a>
+          </li>`
     console.log(listItems)
     }   
 ulEl.innerHTML = listItems
